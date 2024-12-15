7e31b74eef73f808138e4b5a6a05070e7c2178fd
Kenneth Murray <kwmwins@gmail.com>
2024-12-14 17:22:52 -0700

    Added Bootstrap nav link

--- Marvel-Heros/src/NavigationBar.jsx
+++ Marvel-Heros/src/NavigationBar.jsx
@@ -1,20 +1,44 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import { Navbar, Nav } from 'react-bootstrap';
+import { NavLink } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default function NavigationBar() {
   return (
-    <Navbar bg="dark" variant="dark" expand="lg">
-      <Navbar.Brand as={Link} to="/">Marvel Universe</Navbar.Brand>
+    <Navbar bg="dark" variant="dark" expand="lg" className="p-3 shadow">
+      <Navbar.Brand as={NavLink} to="/" className="fs-4 fw-bold text-uppercase text-warning">
+        Marvel Universe
+      </Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className="me-auto">
-          <Nav.Link as={Link} to="/">Home Page</Nav.Link>
-          <Nav.Link as={Link} to="/character-list">Browse Characters</Nav.Link>
-          <Nav.Link as={Link} to="/character-details">Character Details</Nav.Link>
+        <Nav className="ms-auto">
+          <Nav.Link 
+            as={NavLink} 
+            to="/" 
+            className="text-light px-3"
+            style={({ isActive }) => (isActive ? { fontWeight: 'bold', color: '#ffc107' } : undefined)}
+          >
+            Home Page
+          </Nav.Link>
+          <Nav.Link 
+            as={NavLink} 
+            to="/character-list" 
+            className="text-light px-3"
+            style={({ isActive }) => (isActive ? { fontWeight: 'bold', color: '#ffc107' } : undefined)}
+          >
+            Browse Characters
+          </Nav.Link>
+          <Nav.Link 
+            as={NavLink} 
+            to="/character-details" 
+            className="text-light px-3"
+            style={({ isActive }) => (isActive ? { fontWeight: 'bold', color: '#ffc107' } : undefined)}
+          >
+            Character Details
+          </Nav.Link>
         </Nav>
       </Navbar.Collapse>
     </Navbar>
   );
 }
+
